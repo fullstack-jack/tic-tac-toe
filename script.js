e50d3b6ae375b52bb2d2ab7588def5d77b9d0202
Jack Temple <220726590+fullstack-jack@users.noreply.github.com>
2025-10-28 16:13:25 +0000

    initialised file structure

---
+++ script.js
@@ -0,0 +1,9 @@
+// 1. Make a gameboard array
+// 2. Make player objects
+// 3. Make object to control the game flow
+// 4. Make it work in the console
+// 5. Once it works in the console, make an object for the DOM logic/have a function to render to the webpage 
+// Clean up the interface to allow players to put in their names, include a button to start/restart the game 
+// and add a display element that shows the results upon game end!
+// (Your main goal here is to have as little global code as possible)
+
